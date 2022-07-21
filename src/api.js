91e2e0c15ec54135c7a1c2c865ae0b2dd5a449ba
Ciprian Cozma <cozma_ciprian19@yahoo.com>
2022-07-21 18:10:33 +0300

    Update api.js
    Deleted since it is sensitive data

--- src/api.js
+++ src/api.js
@@ -1,12 +1,1 @@
-export const apiOptions = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "e8191cc510msh1caa602240b6a36p1cc4d9jsnae1565bd52b0",
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-  },
-};
 
-export const API_KEY_WEATHER_FORECAST = "f05b0db6ca6f91f04d495a26f42e65ec";
-
-export const API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
-export const WEATHER_FORECAST = "https://api.openweathermap.org/data/2.5";
